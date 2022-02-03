c18c934145b9c0eabda0d619b47ab325e2123fa2
Tanmoy Das <tanmoydas1997@gmail.com>
2022-02-03 21:22:18 +0530

    Generalise the getDegree function

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const hourHand = document.querySelector(".hour-hand")
 const minuteHand = document.querySelector(".minute-hand")
 const secondHand = document.querySelector(".second-hand")
 
-function getHourDegree(hour) {
-	return (hour * (360 / 12) + 90) % 360
-}
-
-function getDegree(value) {
-	return (value * (360 / 60) + 90) % 360
+function getDegree(value, segments) {
+	return (value * (360 / segments) + 90) % 360
 }
